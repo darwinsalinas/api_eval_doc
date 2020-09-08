1e8c572b4a6d862d4efed2447f833eef80915835
Darwin Salinas <salinash2000@gmail.com>
2020-09-07 23:53:14 -0600

    modelos para la evaluacion

--- encuesta/models.py
+++ encuesta/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from academia.models import Curso
+
+
+class Encuesta(models.Model):
+    titulo = models.CharField(max_length=100)
+    fecha = models.DateField()
+    curso = models.ForeignKey(Curso, models.CASCADE)
+
+
+class Pregunta(models.Model):
+    encuesta = models.ForeignKey(Encuesta, models.CASCADE)
+    pregunta = models.CharField(max_length=200)
+
+
+class Opcion(models.Model):
+    pregunta = models.ForeignKey(Pregunta, models.CASCADE)
+    opcion = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
