73285c2ea9d92d9892e3e872b220409c5221c917
Darwin Salinas <salinash2000@gmail.com>
2020-09-07 23:40:21 -0600

    nuevas relaciones

--- academia/models.py
+++ academia/models.py
@@ -11,9 +11,30 @@ class Persona(models.Model):
     def __str__(self):
         return self.nombres + ' ' + self.apellidos
 
+
+class Catalogo(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    class Meta:
+        abstract=True
+
+    def __str__(self):
+        return self.nombre
+
+
+class Asignatura(Catalogo):
+    pass
+
+
 class Docente(Persona):
     pass
 
 
 class Estudiante(Persona):
     pass
+
+
+class Curso(models.Model):
+    asignatura = models.ForeignKey(Asignatura, models.CASCADE)
+    docente = models.ForeignKey(Docente, models.CASCADE)
+    estudiantes = models.ManyToManyField(Estudiante)
