dcbba40560c3234f9daa9aa9c9191b5201432926
Darwin Salinas <salinash2000@gmail.com>
2020-11-11 16:21:26 -0600

    jwt

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from encuesta.views import EncuestaViewSet, PreguntaViewSet, OpcionViewSet, PreguntaSingle
 from academia.views import DocenteViewSet, EstudianteViewSet, CursoViewSet
-
 
 router = routers.DefaultRouter()
 router.register(r'encuestas', EncuestaViewSet)
@@ -17,5 +21,7 @@ router.register(r'cursos', CursoViewSet)
 urlpatterns = [
     path('', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('api/preguntas/<int:pk>', PreguntaSingle.as_view()),
 ]
