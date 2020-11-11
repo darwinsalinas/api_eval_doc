7ead621e6564f20b6372f2ac7afddaef67de67d1
Darwin Salinas <salinash2000@gmail.com>
2020-11-11 17:16:44 -0600

    protecting url wiht auth

--- academia/views.py
+++ academia/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 
 from .models import Docente, Estudiante, Curso
 from .serializers import DocenteSerializer, EstudianteSerializer, CursoSerializer
 
 class DocenteViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Docente.objects.all()
     serializer_class = DocenteSerializer
 
 
 class EstudianteViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Estudiante.objects.all()
     serializer_class = EstudianteSerializer
 
 
 class CursoViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
--- encuesta/views.py
+++ encuesta/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
 
 from .models import Encuesta, Pregunta, Opcion
 from .serializers import EncuestaSerializer, PreguntaSerializer, OpcionSerializer
 
 class EncuestaViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Encuesta.objects.all()
     serializer_class = EncuestaSerializer
 
